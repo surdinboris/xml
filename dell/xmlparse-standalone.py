e01ba8acd261bdb321673ed3201c22914aeeb80e
borrik <borrik@live.ru>
2018-12-06 14:24:25 +0200

    refactoring to handle reporting and two known types of inventory

--- dell/xmlparse-standalone.py
+++ dell/xmlparse-standalone.py
@@ -8,89 +8,72 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 import datetime
+ascii_uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+import xlsxwriter
+master = 'HardwareInventory.master'
 
 def getdata(xml,classname, name, rawsearch=None):
     listval = []
+
     with open(xml, 'r') as x:
         data = x.read()
     root = ET.fromstring(data)
-
-
-    inst = root.findall('DECLARATION/DECLGROUP/VALUE.OBJECT/INSTANCE')
-
+    #patch to use both two types of xml retrieved via web interface and racadmin
+    inst = root.findall('Component')
+    classnameattr = 'Classname'
+    if len(root.findall('MESSAGE')) == 1:
+        inst = root.findall('MESSAGE/SIMPLEREQ/VALUE.NAMEDINSTANCE/INSTANCE')
+        classnameattr = 'CLASSNAME'
+    #searching for items
     for i in inst:
-        if name == 'RawResult':  #searching in linear PCI inventory data
-            if i.attrib['CLASSNAME'] == classname:
-                vals=i.findall('PROPERTY.ARRAY/VALUE.ARRAY/VALUE')
-                for val in vals:
-                    if val.text and val.text.find(rawsearch) != -1:
-                        listval.append(val.text)
+       # gathering results for regular data
+        if i.attrib[classnameattr] == classname:
+            props=i.findall('PROPERTY')
+            for prop in props:
+                if prop.attrib['NAME'] == name:
+                    val= prop.find('VALUE').text
+                    listval.append(val)
 
-                    if val.text and val.text.find('[SN]')!= -1 and len(listval)>0 and len(listval)%2 !=0:
-                        listval.append(val.text)
-
-        if name == 'RawResults':  # gathering results for raw pci data
-            if i.attrib['CLASSNAME'] == classname:
-                vals=i.findall('PROPERTY.ARRAY/VALUE.ARRAY/VALUE')
-                for val in vals:
-                    if val.text.find(rawsearch) == 0:
-                        if val.text not in listval:
-                             listval.append(val.text)
-
-        if name == 'Name' and rawsearch:  # gathering results for FRU data from two matching values inside one instance
-            if i.attrib['CLASSNAME'] == classname:
-                props = i.findall('PROPERTY')
-                for prop in props:
-                    if prop.attrib['NAME'] == name and prop.find('VALUE').text == rawsearch:
-                        for prop in props:
-                            if prop.attrib['NAME'] == 'SerialNumber':
-                                val = prop.find('VALUE').text
-                                listval.append(val)
-
-        else:  # gathering results for regular data
-            if i.attrib['CLASSNAME'] == classname:
-                props=i.findall('PROPERTY')
-                for prop in props:
-                    if prop.attrib['NAME'] == name:
-                        val=prop.find('VALUE').text
-                        listval.append(val)
     return(listval[0] if len(listval)==1 else listval)
 
 
 def main(argv):
-    inputfile = ''
+    inputdir = ''
     outputdir = ''
     try:
         opts, args = getopt.getopt(argv, "hi:o:", ["ifile=", "ofile="])
     except getopt.GetoptError:
-        print('test.py -i <inputfile> -o <outputdir>')
+        print('test.py -i <inputdir> -o <outputdir>')
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
-            print('test.py -i <inputfile> -o <outputdir>')
+            print('test.py -i <inputdir> -o <outputdir>')
             sys.exit()
-        elif opt in ("-i", "--ifile"):
-            inputfile = arg
-        elif opt in ("-o", "--ofile"):
+        elif opt in ("-i", "--inpdir"):
+            inputdir = arg
+        elif opt in ("-o", "--outpdir"):
             outputdir = arg
-    print('Input file:', inputfile)
+    print('Input file:', inputdir)
     print('Report outputdir:', outputdir)
-    filedetect(inputfile,outputdir)
+    filesProcessing(inputdir, outputdir)
 
-def filedetect(inputdir,outputdir):
-    filelist=[]
+def filesProcessing(inputdir, outputdir):
+    masterRepo = report(os.path.join(inputdir, master))
+    print('Master report generated from HardwareInventory.master \n', masterRepo)
     for inputfile in os.listdir(inputdir):
         fn, ext = (os.path.splitext(inputfile))
-        # if ext == '.xml':
-        #     print('Found xml files:', fn)
-        #     print('Processing files...')
-        #     report(os.path.join(inputdir,inputfile),outputdir)
-        if ext == '.gz':
-            print('Found archived file:', fn)
-            filelist.append(os.path.join(inputdir,inputfile))
-    latest_file = max(filelist, key=os.path.getctime)
-    print('Latest DSA file is:', latest_file)
-    report(unpack(os.path.join(inputdir,latest_file)),outputdir)
+        if ext == '.xml':
+            workbook = xlsxwriter.Workbook(os.path.join(outputdir, os.path.join(inputdir,inputfile)) + '_report.xlsx')
+            worksheet = workbook.add_worksheet()
+            print('Found xml files:', fn)
+            print('Processing files...')
+            #report generation
+            repo = report(os.path.join(inputdir, inputfile))
+
+            writetoxlsx(worksheet, repo, geometry='columns')
+            workbook.close()
+            # reportfile.close()
+            # sendrep(sysserial)
 
 def unpack(latest_file):
     epath, tail =os.path.split(latest_file)
@@ -114,108 +97,157 @@ def unpack(latest_file):
                 #fn, ext = (os.path.splitext(f))
                 if os.path.splitext(f)[1] == '.xml':
                     return(os.path.join(epath,f))
+#columns
+def writetoxlsx(worksheet, results, geometry='rows'):
+    maxwidth = {}
+    #helper to calculate and update with for column
+    def toStr(val, coord):
+        try:
+            curr = maxwidth[coord[0]]
+            if curr < len(val):
+                maxwidth[coord[0]] = len(val)
+        except KeyError:
+            maxwidth[coord[0]] = len(val)
+        return str(val)
+    if geometry == "columns":
+        for i, result in enumerate(results, 0):
+            print(i, result,ascii_uppercase[i])
+            for r in result:
+                #header
+                coords='{}1'.format(ascii_uppercase[i])
+                worksheet.write(coords, toStr(r,coords))
+                #in case of multiple values data
+                if type(result[r]) == list and len(result[r]) > 1 :
+                    for ind, v in enumerate(result[r],2):
+                        coords = '{}{}'.format(ascii_uppercase[i], ind)
+                        worksheet.write(coords, toStr(v, coords))
+                else:
+                    coords = '{}2'.format(ascii_uppercase[i])
+                    worksheet.write(coords, toStr(result[r], coords))
+    if geometry == 'rows':
+        for i, result in enumerate(results, 1):
+            print(i, result)
+            for r in result:
+                # header
+                coords = 'A{}'.format(i)
+                worksheet.write(coords, toStr(r, coords))
+                # in case of multiple values data
+                if type(result[r]) == list and len(result[r]) > 1:
+                    for ind, v in enumerate(result[r]):
+                        # need to enumerate with letters ascii_uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+                        coords = '{}{}'.format(ascii_uppercase[ind + 1], i)
+                        worksheet.write(coords, toStr(v, coords))
+                else:
+                    coords = 'B{}'.format(i)
+                    worksheet.write(coords, toStr(result[r], coords))
+    #sheet setup for better look
+    for m in maxwidth:
+        worksheet.set_column('{}:{}'.format(m,m), maxwidth[m])
 
 
-def report(xml,outputdir):
-    sysserial = getdata(xml, classname='IBMSG_ComputerSystem', name='SerialNumber')
-    dsabuild  =getdata(xml, classname='IBMSG_DsaVPD', name='BuildNumber')
-    dsaver  =getdata(xml, classname='IBMSG_DsaVPD', name='Version')
-    ipmibuild  =getdata(xml, classname='IBMSG_IPMIFirmwareElement', name='BuildNumber')
-    ipmiver  = getdata(xml, classname='IBMSG_IPMIFirmwareElement', name='Version')
-    uefibuild = getdata(xml, classname='IBMSG_SystemBIOSElement', name='BuildNumber')
-    uefiver = getdata(xml, classname='IBMSG_SystemBIOSElement', name='Version')
-
-    raidserial = getdata(xml, classname='LSIESG_PhysicalCard', name='SerialNumber')
-    raidfw = getdata(xml, classname='LSIESG_FirmwarePackageIdentity', name='VersionString')
-
-    drslot = getdata(xml, classname='LSIESG_PhysicalDrive', name='Slot_No')
-    drpartnumber = getdata(xml, classname='LSIESG_PhysicalDrive', name='PartNumber')
-    drserial = getdata(xml, classname='LSIESG_PhysicalDrive', name='SerialNumber')
-    disklist = zip(drslot, drpartnumber, drserial)
-
-    ethname = getdata(xml, classname='IBMSG_BcmDeviceFirmwareElement', name='Name')
-
-    ethfw = getdata(xml, classname='IBMSG_BcmDeviceFirmwareElement', name='Version')
-    ethlist = zip(ethname,ethfw)
-
-    mellanoxlist = getdata(xml, classname='IBMSG_PCIRawData', name='RawResult', rawsearch='Mellanox Technologies Device')
-
-    qlogicser = getdata(xml, classname='IBMSG_QLogicFibreChannelRawData', name='RawResults',rawsearch='Serial Number')
-    sbserial = getdata(xml, classname='IBMSG_IPMIFRU', name='Name',rawsearch='System Board')
-    psu1serial = getdata(xml, classname='IBMSG_IPMIFRU', name='Name',rawsearch='Power Supply 1')
-    psu2serial = getdata(xml, classname='IBMSG_IPMIFRU', name='Name', rawsearch='Power Supply 2')
-    bpserial = getdata(xml, classname='IBMSG_IPMIFRU', name='Name', rawsearch='DASD Backplane 1')
-    pcilist = getdata(xml, classname='IBMSG_PCIDevice', name='Description')
-
-    reportfile = open(os.path.join(outputdir, xml) +'_report.log', "w")
-
-    reportfile.write('{0}Parsing logfile {1} started{0}\n'.format('*' * 20, xml))
-    reportfile.write('System serial number: {0}\n'.format(sysserial))
-    reportfile.write('DSA OEM: {0} {1}\n'.format(dsabuild, dsaver))
-    reportfile.write('IPMI fw: {0} {1}\n'.format(ipmibuild, ipmiver))
-    reportfile.write('UEFI fw: {0} {1}\n'.format(uefibuild, uefiver))
-    reportfile.write('RAID serial number: {0} firmware: {1}\n'.format(raidserial, raidfw))
-    reportfile.write('Board serial number: {0}\n'.format(sbserial))
-    reportfile.write('PSU1 serial number: {0}\n'.format(psu1serial))
-    reportfile.write('PSU2 serial number: {0}\n'.format(psu2serial))
-    reportfile.write('Backplane serial number: {0}\n'.format(bpserial))
+# #rows
+# def writetoxlsxRow(worksheet, results):
+#     maxwidth = {}
+#     #helper to calculate and update with for column
+#     def toStr(val, coord):
+#         try:
+#             curr = maxwidth[coord]
+#             if curr < len(val):
+#                 maxwidth[coord] = len(val)
+#         except KeyError:
+#             maxwidth[coord] = len(val)
+#         return str(val)
+#
+#     #processing results
+#     for i, result in enumerate(results, 1):
+#         print(i, result)
+#         for r in result:
+#             #header
+#             coords = 'A{}'.format(i)
+#             worksheet.write(coords, toStr(r,coords))
+#             # in case of multiple values data
+#             if type(result[r]) == list and len(result[r]) > 1 :
+#                 for ind, v in enumerate(result[r]):
+#                     #need to enumerate with letters ascii_uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+#                     coords='{}{}'.format(ascii_uppercase[ind+1],i)
+#                     worksheet.write(coords, toStr(v,coords))
+#             else:
+#                 coords = 'B{}'.format(i)
+#                 worksheet.write(coords, toStr(result[r], coords))
+#
+#     #sheet setup for better look
+#     for m in maxwidth:
+#         worksheet.set_column('{}:{}'.format(m,m), maxwidth[m])
 
 
-    if len(mellanoxlist)==4:
-        reportfile.write('Mellanox serial numbers: {0} and {1}\n'.format(mellanoxlist[1][23:42],mellanoxlist[3][23:42]))
+#report generation
+def report(xml):
+    #ol stuff
+    #reportfile = open(os.path.join(outputdir, xml) +'_report.log', "w")
+    #reportfile.write('{0}Parsing logfile {1} started{0}\n'.format('*' * 20, xml))
+    #reportfile.write('System serial number: {0}\n'.format(sysserial))
+    #reportfile.write('System CPUs model: {0}\n'.format(cpusmodel))
+    results = []
+    #workbook.add_format()
+    #xls - add data
+    results.append({'ServiceTag': getdata(xml, classname='DCIM_SystemView', name='ServiceTag')})
+    results.append({'CPU model': getdata(xml, classname='DCIM_CPUView', name='Model')})
+    #PCI
+    results.append({'PCI device': getdata(xml, classname='DCIM_PCIDeviceView', name='Description')})
+    #Memory
+    results.append({'System memory size': getdata(xml, classname='DCIM_SystemView', name='SysMemTotalSize')})
+    results.append({'Memory serial': getdata(xml, classname='DCIM_MemoryView', name='SerialNumber')})
+    results.append({'Memory module part number': getdata(xml, classname='DCIM_MemoryView', name='PartNumber')})
+    results.append({'Memory slot': getdata(xml, classname='DCIM_MemoryView', name='FQDD')})
+    #HDD
+    results.append({'HDD serial': getdata(xml, classname='DCIM_PhysicalDiskView', name='SerialNumber')})
+    results.append({'HDD model': getdata(xml, classname='DCIM_PhysicalDiskView', name='Model')})
+    results.append({'HDD fw': getdata(xml, classname='DCIM_PhysicalDiskView', name='Revision')})
+    results.append({'HDD slot population': getdata(xml, classname='DCIM_PhysicalDiskView', name='Slot')})
+    #PSU
+    results.append({'PSU part number': getdata(xml, classname='DCIM_PowerSupplyView', name='PartNumber')})
+    results.append({'PSU serial': getdata(xml, classname='DCIM_PowerSupplyView', name='SerialNumber')})
+    results.append({'PSU model': getdata(xml, classname='DCIM_PowerSupplyView', name='Model')})
+    results.append({'PSU fw': getdata(xml, classname='DCIM_PowerSupplyView', name='FirmwareVersion')})
+    return results
 
-    for disk in disklist:
-        reportfile.write('Drive slot:{0} P/N: {1} serial: {2}\n'.format(disk[0],disk[1],disk[2]))
-    for eth in ethlist:
-        ethd = eth[0].replace(":", "")
-        reportfile.write('Ethernet device: {0} firmware: {1}\n'.format(ethd, eth[1]))
-    for qlogic in qlogicser:
-        reportfile.write('Qlogic serial number: {0}\n'.format(qlogic))
-    reportfile.write('=' * 40+'\n')
-    for pci in pcilist:
-        reportfile.write('PCI device: {0}\n'.format(pci))
 
-    reportfile.close()
-    #sendrep(sysserial)
-    return sysserial,(xml+ '_report.log')
-
-
-def sendrep(sysserial):
-    try:
-        curtime = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
-        fromaddr = "jade@nextra01.xiv.ibm.com"
-        toaddrs = ['IBM-IVT@tel-ad.co.il']
-        subject = "Afterloan server " + sysserial + " test ended " + str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-        msg = MIMEMultipart()
-        msg["From"] = fromaddr
-        msg["To"] = ",".join(toaddrs)
-        msg["Subject"] = subject
-        html = """	
-        <html>
-        <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-        </head>
-        <body>
-        <strong><b>Afterloan server passed:</b></strong>
-        """ + sysserial + """ 
-        <p><u>Log files were transferred to wiki, please follow the link below:</u><br>
-        <a href="http://10.148.38.142/wiki/doku.php?id=lenovo:x3650m5:"""+ sysserial +"""">http://10.148.38.142/wiki/doku.php?id=lenovo:x3650m5:""" + sysserial + """ </a> 
-        <p>Using """ + os.path.realpath(__file__) +""" <p>
-        <p>Generated at: """ + curtime + """
-        <p>Tel-Ad IVT Team.<br>
-        All Rights Reserved to Tel-Ad Electronics LTD. © 2017 
-        </body></html>
-        """
-        msg.attach(MIMEText(html, 'html'))
-        server = smtplib.SMTP()
-        server.connect('localhost')
-        # server.send_message(msg)
-        text = msg.as_string()
-        server.sendmail(fromaddr, toaddrs, text)
-        server.quit()
-    except:
-       if ConnectionRefusedError():
-           print('SMTP connection error, please check network and local Sendmail server')
+# def sendrep(sysserial):
+#     try:
+#         curtime = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
+#         fromaddr = "jade@nextra01.xiv.ibm.com"
+#         toaddrs = ['IBM-IVT@tel-ad.co.il']
+#         subject = "Afterloan server " + sysserial + " test ended " + str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+#         msg = MIMEMultipart()
+#         msg["From"] = fromaddr
+#         msg["To"] = ",".join(toaddrs)
+#         msg["Subject"] = subject
+#         html = """
+#         <html>
+#         <head>
+#         <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+#         </head>
+#         <body>
+#         <strong><b>Afterloan server passed:</b></strong>
+#         """ + sysserial + """
+#         <p><u>Log files were transferred to wiki, please follow the link below:</u><br>
+#         <a href="http://10.148.38.142/wiki/doku.php?id=lenovo:x3650m5:"""+ sysserial +"""">http://10.148.38.142/wiki/doku.php?id=lenovo:x3650m5:""" + sysserial + """ </a>
+#         <p>Using """ + os.path.realpath(__file__) +""" <p>
+#         <p>Generated at: """ + curtime + """
+#         <p>Tel-Ad IVT Team.<br>
+#         All Rights Reserved to Tel-Ad Electronics LTD. © 2017
+#         </body></html>
+#         """
+#         msg.attach(MIMEText(html, 'html'))
+#         server = smtplib.SMTP()
+#         server.connect('localhost')
+#         # server.send_message(msg)
+#         text = msg.as_string()
+#         server.sendmail(fromaddr, toaddrs, text)
+#         server.quit()
+#     except:
+#        if ConnectionRefusedError():
+#            print('SMTP connection error, please check network and local Sendmail server')
 
 if __name__ == "__main__":
     main(sys.argv[1:])
